85688aa0cbd8490feaad090040c119f0ce3482c6
Kilian Trunk <hunterteammovies@gmail.com>
2023-07-05 15:05:46 +0200

    Updates copying of files

--- copy-files.js
+++ copy-files.js
@@ -1,24 +1,11 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-// Define the files to be copied
-const filesToCopy = [
-  {
-    from: "node_modules/gameart-task-kilian/js-functions.js",
-    to: "js-functions.js",
-  },
-  { from: "node_modules/gameart-task-kilian/ping-pong.js", to: "ping-pong.js" },
-  {
-    from: "node_modules/gameart-task-kilian/ping-pong.test.js",
-    to: "ping-pong.test.js",
-  },
-];
+// Define the source and destination paths
+const sourcePath = path.resolve(__dirname, "node_modules/gameart-task-kilian/");
+const destinationPath = path.resolve(__dirname);
 
-// Copy files to the root directory
-filesToCopy.forEach((file) => {
-  const sourcePath = path.resolve(__dirname, file.from);
-  const destinationPath = path.resolve(__dirname, file.to);
-  fs.copySync(sourcePath, destinationPath);
-});
+// Copy all files from the source to the destination
+fs.copySync(sourcePath, destinationPath);
 
 console.log("Files copied successfully!");
