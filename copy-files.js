1adfd95efa2d1046889c4df807f7dfda20ce2875
Kilian Trunk <hunterteammovies@gmail.com>
2023-07-05 15:14:31 +0200

    updates copying of files

--- copy-files.js
+++ copy-files.js
@@ -6,6 +6,12 @@ const sourcePath = path.resolve(__dirname, "node_modules/gameart-task-kilian/");
 const destinationPath = path.resolve(__dirname);
 
 // Copy all files from the source to the destination
-fs.copySync(sourcePath, destinationPath);
+fs.copySync(sourcePath, destinationPath, {
+  filter: (src) => {
+    // Exclude the 'node_modules/gameart-task-kilian' directory
+    const relativePath = path.relative(sourcePath, src);
+    return relativePath !== "node_modules/gameart-task-kilian";
+  },
+});
 
 console.log("Files copied successfully!");
